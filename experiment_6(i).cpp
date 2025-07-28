fcf608cb7a526e60332b107219b14f0c86412194
navya <navyapkhajuria@gmail.com>
2025-07-28 15:12:18 +0530

    Update experiment_6(i).cpp

--- experiment_6(i).cpp
+++ experiment_6(i).cpp
@@ -1,3 +1,7 @@
+// Navya Pawan Khajuria 
+// 24070123151
+// B3
+
 #include <iostream>
 using namespace std;
 
@@ -9,3 +13,6 @@ int main() {
 
     return 0;
 }
+
+//Output:
+//1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 
